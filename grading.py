00db210101b3f11ad6a2f342a7a4977a396cb7dc
hyesungoh <haesungoh414@gmail.com>
2021-03-23 21:19:04 +0900

    get problems with stdin, get student's answers with selenium, bs4

--- grading.py
+++ grading.py
@@ -1,16 +1,10 @@
-# rom selenium import webdriver
-#
-# driver = webdriver.Chrome('chromedriver')
-# driver.get(URL)
-# ...
-#
-# driver.close()
-
-
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import csv
 
 CSV_FILE_NAME = "boj_id_csv.csv"
+GRADING_FILE_NAME = "grading.md"
+BOJ_URL = "https://www.acmicpc.net/user/"
 
 def return_student_information():
     file = open(CSV_FILE_NAME, "r", encoding='utf-8')
@@ -24,9 +18,24 @@ def return_student_information():
     file.close()
     return student_information
 
+def grading(student_id, problems):
+    driver.get(BOJ_URL + student_id)
+    page_source = BeautifulSoup(driver.page_source, "html.parser")
+    driver.close()
 
-student_information = return_student_information()
-print(student_information)
-# driver = webdriver.Chrome('chromedriver')
-# driver.get("https://www.naver.com")
+    correct_div = page_source.find("div", {"class": "panel-body"})
+    students_answers = []
+    for answer in correct_div.findAll("a"):
+        students_answers.append(answer.get_text())
+
+    for problem in problems:
+        if problem not in students_answers:
+            return False
+    return True
+
+    
+driver = webdriver.Chrome('chromedriver')
+problems = input().split()
+# student_information = return_student_information()
+grading("hs980414", problems)
 
